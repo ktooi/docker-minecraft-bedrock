4b929217f7c0105c1ae696b6e9e4e6ac4a38b859
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2020-03-21 13:42:38 +0900

    Added a function that ask agreed to MEULA.

--- bedrock_server.sh
+++ bedrock_server.sh
@@ -3,6 +3,7 @@
 set -ue
 
 BASE_DIR=$(cd $(basename "$0"); pwd)
+: ${I_AGREE_TO_MEULA_AND_PP:=""}
 
 __BEDROCK_SERVER_URL=""
 __BEDROCK_SERVER_URL_VER_PAT='https:\/\/minecraft\.azureedge\.net\/bin-linux\/bedrock-server-([0-9]+\.[0-9]+\.[0-9]+\.[0-9])\.zip'
@@ -27,8 +28,21 @@ function has_bedrock_server_latest_file() {
 	[ -e ${BASE_DIR}/$(get_bedrock_server_latest_filename) ]
 }
 
+function ask_agree_meula() {
+	while [ "${I_AGREE_TO_MEULA_AND_PP}" != "yes" -a "${I_AGREE_TO_MEULA_AND_PP}" != "no" ]
+	do
+		read -p "I agree to Minecraft End User License Agreement and Privacy Policy. (yes/no): " I_AGREE_TO_MEULA_AND_PP
+	done
+}
+
 function download_bedrock_server_latest_file() {
-	wget $(get_bedrock_server_url) -O ${BASE_DIR}/$(get_bedrock_server_latest_filename)
+	ask_agree_meula
+	if [ "${I_AGREE_TO_MEULA_AND_PP}" == "yes" ]; then
+		wget $(get_bedrock_server_url) -O ${BASE_DIR}/$(get_bedrock_server_latest_filename)
+	else
+		echo "You must agree to Minecraft End User License Agreement and Privacy Policy."
+		return 1
+	fi
 }
 
 function build_docker_image() {
@@ -39,6 +53,9 @@ function build_docker_image() {
 }
 
 function main() {
+	if [ "${1:-""}" == "--i-agree-to-meula-and-pp" ]; then
+		I_AGREE_TO_MEULA_AND_PP="yes"
+	fi
 	if has_bedrock_server_latest_file; then
 		: # Nothing to do.
 	else
